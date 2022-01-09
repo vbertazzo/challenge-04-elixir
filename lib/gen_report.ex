0474da60cd53a8b1b6908b75b2c82ddd3d9f97a4
vbertazzo <vbertazzo@gmail.com>
2022-01-08 22:06:51 -0300

    Add build_from_many function

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,122 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  @workers [
+    "daniele",
+    "mayk",
+    "giuliano",
+    "cleiton",
+    "jakeliny",
+    "joseph",
+    "diego",
+    "danilo",
+    "rafael",
+    "vinicius"
+  ]
+
+  @month_names [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  def build(), do: {:error, "Insira o nome de um arquivo"}
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Insire uma lista de arquivos"}
+  end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
+  defp sum_values(
+         [worker, hours, _day, month, year],
+         %{
+           "all_hours" => all_hours,
+           "hours_per_month" => hours_per_month,
+           "hours_per_year" => hours_per_year
+         } = report
+       ) do
+    all_hours = Map.put(all_hours, worker, all_hours[worker] + hours)
+
+    individual_hours_per_month =
+      Map.put(hours_per_month[worker], month, hours_per_month[worker][month] + hours)
+
+    hours_per_month = Map.put(hours_per_month, worker, individual_hours_per_month)
+
+    individual_hours_per_year =
+      Map.put(hours_per_year[worker], year, hours_per_year[worker][year] + hours)
+
+    hours_per_year = Map.put(hours_per_year, worker, individual_hours_per_year)
+
+    %{
+      report
+      | "all_hours" => all_hours,
+        "hours_per_month" => hours_per_month,
+        "hours_per_year" => hours_per_year
+    }
+  end
+
+  defp sum_reports(
+         %{
+           "all_hours" => all_hours1,
+           "hours_per_month" => hours_per_month1,
+           "hours_per_year" => hours_per_year1
+         },
+         %{
+           "all_hours" => all_hours2,
+           "hours_per_month" => hours_per_month2,
+           "hours_per_year" => hours_per_year2
+         }
+       ) do
+    all_hours = Map.merge(all_hours1, all_hours2, fn _key, value1, value2 -> value1 + value2 end)
+    hours_per_month = deep_map_merge(hours_per_month1, hours_per_month2)
+    hours_per_year = deep_map_merge(hours_per_year1, hours_per_year2)
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
+  end
+
+  defp deep_map_merge(map1, map2) do
+    Map.merge(map1, map2, fn _key, outer_value1, outer_value2 ->
+      Map.merge(outer_value1, outer_value2, fn _key, inner_value1, inner_value2 ->
+        inner_value1 + inner_value2
+      end)
+    end)
+  end
+
+  defp report_acc do
+    all_hours = Enum.into(@workers, %{}, &{&1, 0})
+    months = Enum.into(@month_names, %{}, &{&1, 0})
+    hours_per_month = Enum.into(@workers, %{}, &{&1, months})
+    years = Enum.into(2016..2020, %{}, &{&1, 0})
+    hours_per_year = Enum.into(@workers, %{}, &{&1, years})
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
---
+++ lib/parser.ex
@@ -0,0 +1,37 @@
+defmodule GenReport.Parser do
+  @month_names %{
+    "1" => "janeiro",
+    "2" => "fevereiro",
+    "3" => "março",
+    "4" => "abril",
+    "5" => "maio",
+    "6" => "junho",
+    "7" => "julho",
+    "8" => "agosto",
+    "9" => "setembro",
+    "10" => "outubro",
+    "11" => "novembro",
+    "12" => "dezembro"
+  }
+
+  def parse_file(filename) do
+    filename
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.downcase()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(3, &get_month_name/1)
+    |> List.update_at(4, &String.to_integer/1)
+  end
+
+  defp get_month_name(month) do
+    @month_names[month]
+  end
+end
